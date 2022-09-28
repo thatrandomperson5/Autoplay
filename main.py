be328d561ae93f0804ab6a9c893bb1ed53c131df
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2022-09-28 17:57:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from flask import Flask, render_template, request, redirect, url_for
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+  if not "id" in request.args:
+    return redirect("/info")
+  else:
+    return redirect(url_for("main", **request.args.to_dict()))
+@app.route("/play")
+def main():
+  return render_template("/index.html")
