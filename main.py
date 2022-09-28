02a714fdc3a7d3b528086f5a7cf9bcbf9e2404ac
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2022-09-28 18:40:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, abort
 import requests, re
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def home():
