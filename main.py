2301046c5d4904033e16efc9d6634807b3782b40
Nilts <94306853+thatrandomperson5@users.noreply.github.com>
2022-09-28 18:32:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, abort
+import requests, re
 
 app = Flask(__name__)
 
@@ -11,3 +12,10 @@ def home():
 @app.route("/play")
 def main():
   return render_template("index.html")
+@app.route("/tools/getVid-<id>")
+def getvid(id):
+  if re.match(r"\A([^"&?/\s]{11})$", id):
+    req = requests.get(f"https://www.youtube.com/embed/{id}?enablejsapi=1&controls=0&disablekb=1&fs=0&modestbranding=1&origin={request.host}&playsinline=1&rel=0")
+    return req.content
+  else:
+    abort(501)
